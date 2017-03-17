095514a7b7ad358fcc2aa1f03c7eee303a520254
wonku <wonkuji@naver.com>
2017-03-16 19:56:58 -0700

    Basin Source commit

---
+++ Cason/Cason/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cason
+//
+//  Created by wonku on 2017. 3. 16..
+//  Copyright © 2017년 wonku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cason/Cason/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cason
+//
+//  Created by wonku on 2017. 3. 16..
+//  Copyright © 2017년 wonku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
