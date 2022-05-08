81280385be48d9458d116f5d2a26575379b8db0c
wonkuji <wonkuji@naver.com>
2022-05-09 00:13:30 +0900

    calculator 5월 8일 스터디 내용 반영

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -15,16 +15,51 @@ class CalculatorBrain {
     func setOperand(operand: Double) {
         accumulator = operand
     }
+ 
+    var operation: Dictionary<String,Operation> = [
+        "Pi" : Operation.Constant(.pi),
+        "e" : Operation.Constant(M_E),
+        "root" : Operation.UnaryOperation,
+        "cos" : Operation.UnaryOperation
+    ]
+    
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation
+        case BinaryOperation
+        case Equals
+    }
+    
     
     func performOperation(symbol: String) {
+//        switch symbol {
+//        case "Pi":
+//            accumulator = Double.pi
+//        case "root":
+//            accumulator = sqrt(accumulator)
+//        default :
+//            break
+//        }
         
-        switch symbol {
-        case "Pi":
-            accumulator = Double.pi
-        case "multiply":
-            accumulator = sqrt(accumulator)
-        default :
-            break
+        
+        if let operation = operation[symbol] {
+                        
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+                break;
+            case .UnaryOperation:
+                
+                break;
+            case .BinaryOperation:
+                
+                break;
+            case .Equals:
+                
+                break;
+            default:
+                break;
+            }
         }
     }
     
